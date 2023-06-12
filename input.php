60accbffec3e27684871b8fd1683e5ca0e8cfa34
Ajay5028 <134187337+Ajay5028@users.noreply.github.com>
2023-06-12 13:24:28 +0530

    Add files via upload

---
+++ input.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+    <form action="practics.php" mathod="get">
+       FIRST NAME=<input type="text"name="ajay"><br><br>
+      LAST NAME= <input type="text"name="a"><br><br>
+       ENROLLMENT NO=<input type="text"name="b"><br><br>
+       SEM=<input type="text"name="c"><br><br>
+       DIV=<input type="text"name="d"><br><br>
+        DEPARTMENT=<input type="text"name="e">
+         <input type="submit"><br><br>
+      
+    </form>
+</body>
+</html>
---
+++ practics.php
@@ -0,0 +1,14 @@
+<?php
+$s1=$_GET["ajay"];
+$s2=$_GET["a"];
+$s3=$_GET["b"];
+$s4=$_GET["c"];
+$s5=$_GET["d"];
+$s6=$_GET["e"];
+echo "first name=   ".$s1."<br>";
+echo "last name=  ".$s2."<br>";
+echo "enrollment=  ".$s3."<br>";
+echo "sem=  ".$s4."<br>";
+echo "div=  ".$s5."<br>";
+echo "departmet=  ".$s6."<br>";
+?>
